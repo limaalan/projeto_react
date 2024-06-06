115727ebaacf38a423e0a2a70a1819f83a26777e
Alan Lima Marques <alanlimamarques6@gmail.com>
2024-06-06 10:31:01 -0300

    feat: criar tabela de listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,32 +1,44 @@
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
+
+import { IListagemPessoa, PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
-import { useEffect, useMemo } from "react";
-import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { useDebounce } from "../../shared/hooks";
 
 export const ListagemDePessoas: React.FC = () => {
-    const {debounce} = useDebounce();
+  const [searchParams , setSearchParams] = useSearchParams();
+  const {debounce} = useDebounce();
 
-    const [searchParams , setSearchParams] = useSearchParams();
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading,setIsLoading] = useState(true);
+
     const busca = useMemo(()=>{
         return searchParams.get('busca') ||''
     },[searchParams]);
     
 
     useEffect(()=>{
+      setIsLoading(true);
 
       debounce(()=>{
-        
+
               PessoasService.getAll(undefined, undefined, busca)
               .then((result)=>{
-                if(result instanceof Error) alert (result.message);
-                else (console.log(result));
-                         
-              })
+                // Finalizou a consulta, muda 'isLoading' pra falso
+                setIsLoading(false);
 
+                if(result instanceof Error){ alert (result.message);}
+                else {
+                  (console.log(result));
+                  setRows(result.data)
+                  setTotalCount(result.totalCount)
+                }
+              })
       })
-      
+
     },[busca])
     
   return (
@@ -39,6 +51,38 @@ export const ListagemDePessoas: React.FC = () => {
                 textoDaBusca={busca}
                 aoMudarTextoDeBusca={texto=>setSearchParams({busca:texto})}
           />}   >
+      <TableContainer component={Paper} variant="outlined" sx={{margin:1 , width:'auto'}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          
+          {rows.map(row=> (
+          
+            <TableRow key = {row.id}>
+              <TableCell>Ações</TableCell>
+              <TableCell>{row.nomeCompleto}</TableCell>
+              <TableCell>{row.email}</TableCell>
+            </TableRow>
+
+          ))}
+
+          </TableBody>
+
+          <TableFooter>
+            {isLoading && (
+              <LinearProgress variant="indeterminate"/>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
+
+
 
     </LayoutBaseDePagina>
   );
