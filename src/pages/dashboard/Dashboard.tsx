332d1f7035ee704f4c3594ce9f7303a5cd358f5c
Alan Lima Marques <alanlimamarques6@gmail.com>
2024-06-12 11:33:06 -0300

    feat: consultar contagem de pessoas para exibir na página inicial

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,46 @@
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDaListagem, FerramentasDeDetalhe } from "../../shared/components";
-import { Box, Card, CardContent, Grid, Paper, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Paper, Typography, debounce } from "@mui/material";
+import { useEffect, useState } from "react";
+import { CidadesService } from "../../shared/services/api/cidades/CidadesService";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 export const Dashboard = () => {
-  return (
+  
+	const [isLoadingCidades,setIsLoadingCidades] = useState(true);
+	const [totalCountCidades, setTotalCountCidades] = useState(0);
+	const [isLoadingPessoas,setIsLoadingPessoas] = useState(true);
+	const [totalCountPessoas, setTotalCountPessoas] = useState(0);
+
+	useEffect(()=>{
+		setIsLoadingCidades(true);
+		CidadesService.getAll(1)
+			.then((result)=>{
+				// Finalizou a consulta, muda 'isLoading' pra falso
+				setIsLoadingCidades(false);
+					if(result instanceof Error){ alert (result.message);}
+					else {
+						(console.log(result));
+						setTotalCountCidades(result.totalCount)
+					}
+			})
+	},[])
+
+	useEffect(()=>{
+		setIsLoadingPessoas(true);
+		PessoasService.getAll(1)
+			.then((result)=>{
+				// Finalizou a consulta, muda 'isLoading' pra falso
+				setIsLoadingPessoas(false);
+					if(result instanceof Error){ alert (result.message);}
+					else {
+						(console.log(result));
+						setTotalCountPessoas(result.totalCount)
+					}
+			})
+	},[])
+	
+	
+	return (
     <LayoutBaseDePagina
       titulo="Sou o dashboard"
       barraDeFerramentas={
@@ -24,7 +62,12 @@ export const Dashboard = () => {
 								<Typography variant="h5" align="center"> Total de Cidades</Typography>
 
 								<Box padding={6} display='flex' justifyContent='center' alignItems='center'>
-									<Typography variant="h1"> 25</Typography>
+									
+									{isLoadingCidades?
+									<Typography variant="h1"> Carregando... </Typography>
+									:<Typography variant="h1"> {totalCountCidades} </Typography>
+									}
+
 								</Box>
 							</CardContent>
 
@@ -38,7 +81,10 @@ export const Dashboard = () => {
 								<Typography variant="h5" align="center"> Total de Pessoas</Typography>
 
 								<Box padding={6} display='flex' justifyContent='center' alignItems='center'>
-									<Typography variant="h1"> 10</Typography>
+								{isLoadingPessoas?
+									<Typography variant="h1"> Carregando... </Typography>
+									:<Typography variant="h1"> {totalCountPessoas} </Typography>
+									}
 								</Box>
 							</CardContent>
 
